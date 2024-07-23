539171b5bad7cd25e6ab00bd2fb795c5f3b0f76f
danifai <102121915+danifai@users.noreply.github.com>
2024-07-23 13:18:28 -0300

    Second commit - advanced

--- public/js/index.js
+++ public/js/index.js
@@ -13,41 +13,64 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('mouseleave', () => {
         cursor.style.display = 'none';
     });
-
-    const inIcon = document.querySelector('.fa-linkedin');
-
     
-    inIcon.addEventListener('mouseover', () => {
-        inIcon.classList.add('fa-bounce');
+    const menuToggle = document.getElementById('menu-toggle');
+    const navContainer = document.getElementById('nav-container');
+    const navLinks = document.querySelectorAll('.nav-link');
+    const showCivil = document.getElementById('show-civil-xp');
+    const civilContainer = document.getElementById('civil-container');
+    const projetctsContainer = document.getElementById("projects");
+    const lastItXp = document.getElementById("last-it-xp");
+    const viewCertificate = document.getElementById("view-certificate");
+    const viewCertificate2 = document.getElementById("view-certificate-2");
+    const modal = document.getElementById("myModal");
+    const modal2 = document.getElementById("myModal-2");
+    const span = document.querySelector("#myModal .close");
+    const spanFullstack = document.querySelector("#myModal-2 .close");
+    const captionText = document.getElementById("caption");
+    const captionText2 = document.getElementById("caption-2");
+    const img = document.getElementById("python-img");
+    const fullstackImg = document.getElementById("fullstack-img");
+
+    menuToggle.addEventListener('click', function() {
+        navContainer.classList.toggle('open');
     });
 
-    
-    inIcon.addEventListener('mouseout', () => {
-        inIcon.classList.remove('fa-bounce');
+    showCivil.addEventListener('click', function() {
+        civilContainer.classList.toggle('show');
+        showCivil.classList.toggle('active');
+
+        if (civilContainer.classList.contains('show')) {
+            projetctsContainer.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+        } else {
+            lastItXp.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+        }
     });
 
-    const gitIcon = document.querySelector('.fa-github');
+    viewCertificate.onclick = function(){
+        modal.style.display = "block";
+        captionText.innerHTML = img.alt;
+    }
 
-    
-    gitIcon.addEventListener('mouseover', () => {
-        gitIcon.classList.add('fa-bounce');
-    });
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
 
-    
-    gitIcon.addEventListener('mouseout', () => {
-        gitIcon.classList.remove('fa-bounce');
-    });
+    viewCertificate2.onclick = function(){
+        modal2.style.display = "block";
+        captionText2.innerHTML = fullstackImg.alt;
+    }
 
-    const envIcon = document.querySelector('.fa-envelope');
+    spanFullstack.onclick = function() {
+        modal2.style.display = "none";
+    }
 
-    
-    envIcon.addEventListener('mouseover', () => {
-        envIcon.classList.add('fa-bounce');
-    });
-
-    
-    envIcon.addEventListener('mouseout', () => {
-        envIcon.classList.remove('fa-bounce');
-    });
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        } else if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+    }
 
 });
